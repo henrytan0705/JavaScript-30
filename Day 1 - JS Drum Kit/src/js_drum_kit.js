ff227578eebd371fbffbce78100abc8ce4bf7665
HenryTan0705 <htan0705@gmail.com>
2020-04-20 15:26:15 -0400

    Clean up code

--- Day 1 - JS Drum Kit/src/js_drum_kit.js
+++ Day 1 - JS Drum Kit/src/js_drum_kit.js
@@ -1,17 +1,25 @@
 document.addEventListener("keydown", (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-
-    console.log(`Pressed ${e.key}`);
-    if (!audio) return;
-    key.classList.add("highlight"); 
-    audio.currentTime = 0;
-    audio.play();
+    playSound(e);
 });
 
 document.addEventListener("keyup", (e) => {
+    removeStyle(e);
+})
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    console.log(`End of press ${e.key}`)
+
+    if (!audio) return;
+    key.classList.add("highlight");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removeStyle(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
     if (!key) return;
     key.classList.remove("highlight");
-})
+}
+
